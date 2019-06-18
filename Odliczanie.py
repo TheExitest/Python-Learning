eaa93d424618060b2af0effd6feb7a205e75f769
TheExitest <theexitest@gmail.com>
2019-06-18 19:21:20 +0200

    Update Odliczanie.py
    Tworzenie pliku i edycja go, działające odliczanie czasu, Input tylko liczby naturalne

--- Odliczanie.py
+++ Odliczanie.py
@@ -1,7 +1,38 @@
+"""
+	Do zrobienia:
+	Odliczanie czasu DONE
+	Input tylko wartosci liczbowe, zmieniac liczby ujemne na naturalne DONE
+	Edycja pliku DONE
+	GUI
+	Tekst do podania	
+"""
 import time
+import datetime
 
 
-def TimeChanger(minuty,sekundy):
+class Podaj_czas:
+	
+	@staticmethod
+	def min_input():
+		while True:
+			try:
+				minuty = abs(int(input("Minut: ")))
+				return minuty
+			except:
+				print ("Wpisz wartość w cyfrach arabskich (0,1,2,3,4,5,6,7,8,9)")
+
+
+	@staticmethod
+	def sec_input():
+		while True:
+			try:
+				sekundy = abs(int(input("Sekund: ")))
+				return sekundy
+			except:
+				print ("Wpisz wartość w cyfrach arabskich (0,1,2,3,4,5,6,7,8,9)")
+
+
+def TimeChanger(minuty, sekundy):
     Sek = int(minuty)*60
     Sek = Sek + int(sekundy)
     return  int(Sek)
@@ -10,22 +41,23 @@ def TimeChanger(minuty,sekundy):
 def odliczanie(odliczacz):
 	a=0
 	czas_pozostaly = odliczacz
-	while(a<odliczacz):
-		a=+1
-		czas_pozostaly+-1
+	czas_pozostaly2 = odliczacz
+	#print(f"Czas pozostały: {czas_pozostaly} Sekund ||Czas systemowy {str(datetime.datetime.now().time())}")
+	while(a<czas_pozostaly):
+		a+=1
+		czas_pozostaly2-=1
 		time.sleep(1)
-	return czas_pozostaly
+		czas_txt=open(nazwa_pliku, "w")
+		czas_txt.write(f"{wiadomosc} {czas_pozostaly2} Sekund")
 
+		#print(f"{czas_pozostaly2} Sekund || Czas systemowy {str(datetime.datetime.now().time())}")
+
+nazwa_pliku = str(input("Podaj nazwe pliku z końcówką .txt "))
+wiadomosc = str(input("Podaj wiadomosc: "))
 print("Podaj czas do odliczenia ")
-minuty = input("Minut: ")
-sekundy = input("Sekund: ")
+minuty = Podaj_czas.min_input()
+sekundy = Podaj_czas.sec_input()
+czas_txt=open(nazwa_pliku, "w")
 
 odliczacz=TimeChanger(minuty,sekundy)
-
-while(odliczanie(odliczacz)>0):
-	print(f"{czas_pozostaly} Sekund")
-
-"""
-print(TimeChanger(minuty,sekundy))
-print(f"Czas do odliczenia: {minuty} Minut i {sekundy} Sekund")
-"""
+print(odliczanie(odliczacz))
